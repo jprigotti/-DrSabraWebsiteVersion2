8f3b0626f2037e5fc1e49947e2624678eef9653d
Juan Pablo Rigotti <rigottijp@gmail.com>
2022-04-27 01:32:41 -0300

    first files

---
+++ parallax.js
@@ -0,0 +1,20 @@
+$(window).scroll(function(e) {
+    parallax();
+  })
+  
+  
+  function parallax() {
+    var scroll = $(window).scrollTop();
+    var screenHeight = $(window).height();
+  
+    $('.parallax--bg').each(function() {
+      var offset = $(this).offset().top;
+      var distanceFromBottom = offset - scroll - screenHeight
+      
+      if (offset > screenHeight && offset) {
+        $(this).css('background-position', 'center ' + (( distanceFromBottom  ) * 0.3) +'px');
+      } else {
+        $(this).css('background-position', 'center ' + (( -scroll ) * 0.3) + 'px');
+      }
+    })
+  }
